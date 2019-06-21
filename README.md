deb2efa106b6731096f19792d48fb1d3d9c1227a
zhangli <674947076@qq.com>
2019-06-21 18:22:16 +0800

    新增上传功能

--- README.md
+++ README.md
@@ -5,8 +5,25 @@
 > cnpm i or yarn install
 ```
 
-#### 本地启动
+### 多环境配置
+
+```js
+const ConfigEnv = process.env.VUE_APP_URL
+let hostEnv = ''
+
+if (ConfigEnv === 'test') {
+  hostEnv = 'http://prod'
+} else if (ConfigEnv === 'prod') {
+  hostEnv = 'http://prod'
+} else {
+  hostEnv = 'http://dev'
+}
+
+export default hostEnv
 ```
+
+#### 本地启动
+```js
 > npm run dev  // 启动开发环境
 
 > npm run dev-test // 启动测试环境
@@ -17,7 +34,7 @@
 
 #### 本地打包
 
-```
+```js
 > npm run build // 打包开发环境
 
 > npm run build-test // 打包测试环境
@@ -27,7 +44,7 @@
 
 > 打包时要分模块打包, 单独打包每个模块, 只需要传入模块文件名即可, 否则会打包所有模块
 
-```
+```js
 // 如果项目中同时开发了两个模块 demo和main，打包时
 
 > npm run build demo
---
+++ upload.js
@@ -0,0 +1,40 @@
+/**
+ *
+ * @Description 压缩文件后上传服务器
+ * @Author 张立
+ * @Created 2019-06-21
+ *
+ */
+const request = require('request')
+const path = require('path')
+const fs = require('fs')
+const AdmZip = require('adm-zip')
+var zip = new AdmZip()
+const buildModule = process.argv[process.argv.length - 1]
+if (buildModule.indexOf('upload.js') > -1) {
+  console.log('请输入上传模块')
+  return
+}
+zip.addLocalFolder('dist')
+zip.writeZip(`${buildModule}.zip`)
+console.log(path.resolve(__dirname, `${buildModule}.zip`))
+console.log('\x1b[32m', '*************************************', '\n')
+console.log('\x1b[32m', `你正在上传${buildModule}压缩包, 请耐心等待中...`, '\n')
+console.log('\x1b[32m', '*************************************', '\n')
+let total = 1
+let time = setInterval(() => {
+  console.log(total++)
+}, 1000)
+var url = 'http://192.168.1.28:3001/upload-single'
+var formData = {
+  file: fs.createReadStream(path.resolve(__dirname, `${buildModule}.zip`))
+}
+request.post({
+  url: url,
+  formData: formData
+}, function (error, response, body) {
+  if (!error && response.statusCode === 200) {
+    console.log('上传成功')
+    clearInterval(time)
+  }
+})
